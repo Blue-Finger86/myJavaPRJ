6147121f932d59f690c750072dbcb9d0272cc19d
SoyoungKim <sgjlq123@naver.com>
2023-03-23 19:37:28 +0900

    second

---
+++ src/main/java/Qz01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Qz01 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.printf("정수를 입력해 주세요 : ");
+        a = s.nextInt();
+
+        if ( (a%=3) == 0 ) {
+            System.out.printf("3의 배수입니다.");
+        }
+
+    }
+
+}
