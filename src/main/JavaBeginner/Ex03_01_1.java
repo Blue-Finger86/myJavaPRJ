7d4e0c156714a315685267da3554a9a639b75e2f
SoyoungKim <sgjlq123@naver.com>
2023-04-03 21:33:53 +0900

    second

--- src/main/JavaBeginner/Ex03_01_1.java
+++ src/main/JavaBeginner/Ex03_01_1.java
@@ -1,8 +1,9 @@
-public class Ex03_01 {
+public class Ex03_01_1 {
     public static void main(String[] args) {
         System.out.printf("100 + 100");
         System.out.printf("\n");
-        System.out.printf("%d", 100 + 100);
+        System.out.printf("%d", 100+100);
         System.out.printf("\n");
     }
+
 }
---
+++ src/main/JavaBeginner/Problem03_01.java
@@ -0,0 +1,7 @@
+public class Problem03_01 {
+    public static void main(String[] args) {
+        System.out.printf("%d + %d = %d", 100, 200, 100+200);
+        System.out.printf("\n");
+        System.out.printf("%d / %d = %d", 100, 200, 100/200);
+    }
+}
