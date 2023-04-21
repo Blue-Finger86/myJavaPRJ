526258977c96e92e865d2acd698eb87369f25184
SoyoungKim <sgjlq123@naver.com>
2023-04-21 21:46:27 +0900

    2023.04.21

---
+++ src/main/CodeUp/Ex1070.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Ex1070 {
+    // 1~12 중 1개의 정수(월) 입력, 계절 이름 출력
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        switch(a) {
+            case 1:
+            System.out.print("winter");
+            break;
+            case 2:
+            System.out.print("winter");
+            break;
+            case 3:
+            System.out.print("spring");
+            break;
+            case 4:
+            System.out.print("spring");
+            break;
+            case 5:
+            System.out.print("spring");
+            break;
+            case 6:
+            System.out.print("summer");
+            break;
+            case 7:
+            System.out.print("summer");
+            break;
+            case 8:
+            System.out.print("summer");
+            break;
+            case 9:
+            System.out.print("fall");
+            break;
+            case 10:
+            System.out.print("fall");
+            break;
+            case 11:
+            System.out.print("fall");
+            break;
+            case 12:
+            System.out.print("winter");
+            break;
+        }
+
+    }
+}
+
+
+// 정답!
+
