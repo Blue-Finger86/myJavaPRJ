4cd4756127e50ca82ab520bbba3724acd02f6fff
SoyoungKim <sgjlq123@naver.com>
2023-04-30 16:07:53 +0900

    2023.04.30

---
+++ src/main/CodeUp/Ex1071.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex1071 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        for(; ;) {
+            int i = sc.nextInt();
+            if(i == 0) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
+// 정답!
