987124b357a09fc6e71d9d509fffcfc4ac150a08
SoyoungKim <sgjlq123@naver.com>
2023-06-08 17:01:25 +0900

    2023.06.08

---
+++ src/main/CodeUp/Ex1576.java
@@ -0,0 +1,10 @@
+class Ex1576_1 {
+    public static void f() {
+        System.out.print("Hello?");
+    }
+}
+public class Ex1576 {
+    public static void main(String[] args) {
+        Ex1576_1.f();
+    }
+}
