8961091e5c34b3ba46c87aa95e116721bc9b427e
SoyoungKim <sgjlq123@naver.com>
2023-06-09 20:24:28 +0900

    2023.06.09

---
+++ src/main/CodeUp/Ex1532.java
@@ -0,0 +1,15 @@
+class Ex1532_1 {
+    public static long f() {
+       return -2147483649L;
+    }
+}
+
+public class Ex1532 {
+    public static void main(String[] args) {
+        System.out.print(Ex1532_1.f());
+    }
+}
+
+
+//아오 이거 어떻게 long으로 출력하냐!!!!
+//?뭔데 저렇게 끝에 L만 붙여주면 되는 거였음? 와 어이없네..ㅋ
